406ae3a04581f667ea0d1392f6472a7d1e3928fb
leonardooka <156699392+leonardooka@users.noreply.github.com>
2024-04-30 11:45:34 -0300

    adding github profile link

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,12 @@ export default function About() {
           <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
               <div className='flex flex-row max-h-1/5'>
+              <a
+                  href="https://github.com/leonardooka?tab=repositories"
+                  target='_blank'
+                  >
                 <img src='./leo4.jpg' className='w-20 mr-7 mb-4 border-2'/>
+                </a>
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                 Hi, I'm Leo.
                 <br className="hidden lg:inline-block" />I love to build amazing
@@ -29,6 +34,12 @@ export default function About() {
                   href="#projects"
                   className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
                   See My Projects
+                </a>
+                <a
+                  href="https://github.com/leonardooka?tab=repositories"
+                  target='_blank'
+                  className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                  My Github Profile
                 </a>
               </div>
             </div>
